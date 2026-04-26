83c57c30ea6b65cdd084cecde0effcaa8c79ed13
afkfym <afkfym1@gmail.com>
2026-04-26 17:44:40 -0500

    Update scriptlist.lua

--- scriptlist.lua
+++ scriptlist.lua
@@ -1,8 +1,17 @@
 return {
- [5130394318] = {
+    [14067600077] = {
+        Name    = "Type Soul",
+        PaidUrl = "https://api.luarmor.net/files/v4/loaders/a901547774f7e8197933c5a0e55ebe80.lua",
+        FreeUrl = nil,
+    },
+    [5130394318] = {
         Name    = "Bizarre Lineage",
         PaidUrl = "https://api.luarmor.net/files/v4/loaders/450f7000609a5ac4f72100319e4fef66.lua",
         FreeUrl = "https://api.luarmor.net/files/v4/loaders/647748b5cef79f79edf9f7fc1c0957a1.lua",
     },
-
+    [9186719164] = {
+        Name    = "Sailor Piece",
+        PaidUrl = nil,
+        FreeUrl = "https://api.luarmor.net/files/v4/loaders/87b715deb9548b36a3d1a0d313d280ab.lua",
+    },
 }
